e737904ea4cb521306e560ad1e6f4992aeab9a2b
Rimantas <belauzas@gmail.com>
2025-02-04 14:15:50 +0200

    Up

---
+++ js/variables/06-numbers.js
@@ -0,0 +1,55 @@
+/*
+NUMBERS
+
+- sveikieji, desimtainiai
+- teigiami, neigiami
+- Infinity, NaN (skaiciau tipo klaida)
+
+*/
+
+const n1 = 1;
+console.log(n1);
+
+const n2 = -5;
+console.log(n2);
+
+const n3 = 3.14;
+console.log(n3);
+
+const n4 = -2.727;
+console.log(n4);
+
+const n5 = Infinity;
+console.log(n5);
+
+const n6 = -Infinity;
+console.log(n6);
+
+const n7 = -999999999999;
+console.log(n7);
+
+const n8 = NaN;
+console.log(n8);
+
+console.log('labas rytas' / 2);
+console.log('labas rytas' * 2);
+console.log('labas rytas' - 2);
+console.log('labas rytas' % 2);
+console.log('labas rytas' + 2);
+
+console.log(7 / 5);
+console.log(7 - 5);
+console.log(7 % 5);
+console.log(7 + 5);
+console.log(7 * 5);
+console.log(7 ** 5);
+
+console.log(2 ** 0);
+console.log(2 ** 1);
+console.log(2 ** 2);
+console.log(2 ** 3);
+console.log(2 ** 4);
+console.log(2 ** 5);
+
+// Math - matematiniu funkciju "biblioteka"
+
