c15bd1d68754ed7b4503300a371dfeeb0107d4b3
Rimantas <belauzas@gmail.com>
2025-02-17 09:51:14 +0200

    Up

--- js/variables/11-array-hof.js
+++ js/variables/11-array-hof.js
@@ -32,3 +32,38 @@ const ggInit = gg
     .map(s => s[0])
     .join('.') + '!';
 console.log(ggInit);
+
+
+const kk = ['Aa1+', 'Bb2-', 'Cc3'];
+
+// 0 -> "pirma simboli"
+// 1 -> "antra simboli"
+// 2 -> "trecia simboli"
+// 3 -> "ketvirta simboli"
+// 4 -> 
+
+const kkresulr = kk.map(s => s[3]);
+console.log(kkresulr);
+
+console.clear();
+
+const luckyNumbers = [0, 1, 5, 7, 13, 28, 66, 69];
+console.log(luckyNumbers);
+
+const bigerLucky = luckyNumbers.map(n => n * 10);
+console.log(bigerLucky);
+
+// 1 - grazinti didesnius uz 20
+// 2 - grazinti didesnius arba lygius 28
+// 3 - grazinti mazesnius uz 50
+// [0, 1, 5, 7, 13, 28, 66, 69] -> [28, 66, 69]
+
+const bigLucky1 = luckyNumbers.filter(n => n > 20);
+console.log(bigLucky1);
+
+const bigLucky2 = luckyNumbers.filter(n => n >= 28);
+console.log(bigLucky2);
+
+const bigLucky3 = luckyNumbers.filter(n => n < 50);
+console.log(bigLucky3);
+
