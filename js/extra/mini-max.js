25b27e8bbd88a825ec044ec8dfa51d7bdf9d2d2c
Rimantas <belauzas@gmail.com>
2025-02-25 11:32:51 +0200

    Up

--- js/extra/mini-max.js
+++ js/extra/mini-max.js
@@ -6,45 +6,71 @@ function didziausiasSkaiciusSarase(list) {
     return 0;
 }
 
-console.assert(
-    didziausiasSkaiciusSarase('pomidoras') === 'Pateikta netinkamo tipo reikšmė.',
-    'Netinkamo tipas: string'
-);
-console.assert(
-    didziausiasSkaiciusSarase(215462) === 'Pateikta netinkamo tipo reikšmė.',
-    'Netinkamo tipas: number'
-);
-console.assert(
-    didziausiasSkaiciusSarase(true) === 'Pateikta netinkamo tipo reikšmė.',
-    'Netinkamo tipas: boolean'
-);
-console.assert(
-    didziausiasSkaiciusSarase(null) === 'Pateikta netinkamo tipo reikšmė.',
-    'Netinkamo tipas: null'
-);
-console.assert(
-    didziausiasSkaiciusSarase(undefined) === 'Pateikta netinkamo tipo reikšmė.',
-    'Netinkamo tipas: undefined'
-);
-console.assert(
-    didziausiasSkaiciusSarase() === 'Pateikta netinkamo tipo reikšmė.',
-    'Netinkamo tipas: undefined (nes nepateiktas parametras)'
-);
-console.assert(
-    didziausiasSkaiciusSarase({}) === 'Pateikta netinkamo tipo reikšmė.',
-    'Netinkamo tipas: object'
-);
+// Tikriname netinkamus duomenu tipus
+{
+    console.assert(
+        didziausiasSkaiciusSarase('pomidoras') === 'Pateikta netinkamo tipo reikšmė.',
+        'Netinkamo tipas: string'
+    );
+    console.assert(
+        didziausiasSkaiciusSarase(215462) === 'Pateikta netinkamo tipo reikšmė.',
+        'Netinkamo tipas: number'
+    );
+    console.assert(
+        didziausiasSkaiciusSarase(true) === 'Pateikta netinkamo tipo reikšmė.',
+        'Netinkamo tipas: boolean'
+    );
+    console.assert(
+        didziausiasSkaiciusSarase(null) === 'Pateikta netinkamo tipo reikšmė.',
+        'Netinkamo tipas: null'
+    );
+    console.assert(
+        didziausiasSkaiciusSarase(undefined) === 'Pateikta netinkamo tipo reikšmė.',
+        'Netinkamo tipas: undefined'
+    );
+    console.assert(
+        didziausiasSkaiciusSarase() === 'Pateikta netinkamo tipo reikšmė.',
+        'Netinkamo tipas: undefined (nes nepateiktas parametras)'
+    );
+    console.assert(
+        didziausiasSkaiciusSarase({}) === 'Pateikta netinkamo tipo reikšmė.',
+        'Netinkamo tipas: object'
+    );
+}
 
-// console.assert(didziausiasSkaiciusSarase([1]) === 1, 'Grazinamas vienintelis masyve esantis skaicius: 1');
-// console.assert(didziausiasSkaiciusSarase([5]) === 5, 'Grazinamas vienintelis masyve esantis skaicius: 5');
-// console.assert(didziausiasSkaiciusSarase([1, 2, 3]) === 3, '!!!!!!');
-// console.assert(didziausiasSkaiciusSarase([-5, 78, 14, 0, 18]) === 78, 'Err: 3');
+// Masyvas, kuriame nera didziausio skaiciaus
+{
+    console.assert(didziausiasSkaiciusSarase([]) === 'Pateiktas sąrašas negali būti tuščias.', 'Tuscias masyvas');
+    console.assert(didziausiasSkaiciusSarase([true]) === 'Pateiktas sąrašas negali būti tuščias.', 'Tuscias masyvas');
+    console.assert(didziausiasSkaiciusSarase([true, 5]) === 5, 'Didziausia reiksme: 5');
+}
 
+// Sarasai su teigiamais (ne neigiami) skaiciais
+{
+    console.assert(didziausiasSkaiciusSarase([1]) === 1, 'Grazinamas vienintelis masyve esantis skaicius: 1');
+    console.assert(didziausiasSkaiciusSarase([5]) === 5, 'Grazinamas vienintelis masyve esantis skaicius: 5');
+    console.assert(didziausiasSkaiciusSarase([3, 2, 1]) === 3, 'Masyvo pirmutinis skaicius: 3');
+    console.assert(didziausiasSkaiciusSarase([1, 3, 2]) === 3, 'Masyvo "vidurinis" skaicius: 3');
+    console.assert(didziausiasSkaiciusSarase([1, 2, 3]) === 3, 'Masyvo paskutinis skaicius: 3');
+    console.assert(didziausiasSkaiciusSarase([78, -5, 14, 0, 18]) === 78, 'Masyvo pirmutinis skaicius: 78');
+    console.assert(didziausiasSkaiciusSarase([-5, 78, 14, 0, 18]) === 78, 'Masyvo "vidurinis" skaicius: 78');
+    console.assert(didziausiasSkaiciusSarase([-5, 14, 0, 18, 78]) === 78, 'Masyvo paskutinis skaicius: 78');
+    console.assert(didziausiasSkaiciusSarase([69, 69, 69, 69]) === 69, 'Masyvas tik su vienodomis reiksmemis');
+    console.assert(didziausiasSkaiciusSarase([69, 69, 69, 69, 66]) === 69, 'Pasikartojanti didziausia reiksme');
+    console.assert(didziausiasSkaiciusSarase([69, 69, 69, 69, 66, 66]) === 69, 'Pasikartojanti didziausia reiksme');
+    console.assert(didziausiasSkaiciusSarase([0, 3.14, 2.72, 4]) === 4, 'Didziausia reiksme: 4');
+    console.assert(didziausiasSkaiciusSarase([0, 3.14, 2.72]) === 4, 'Didziausia reiksme: 3.14');
+}
 
-
-// console.assert(didziausiasSkaiciusSarase([69, 69, 69, 69, 66]) === 69, 'Err: 4');
-// console.assert(didziausiasSkaiciusSarase([-1, -2, -3, -4, -5, -6, -7, -8]) === -1, 'Err: 5');
-// console.assert(didziausiasSkaiciusSarase([]) === 'Pateiktas sąrašas negali būti tuščias.', 'Err: 7');
+// Sarasai su neigiamais (ne teigiami) skaiciais
+{
+    console.assert(didziausiasSkaiciusSarase([-1, -2, -3, -4, -5, -6, -7, -8]) === -1, 'Didziausia reiksme: -1');
+    console.assert(didziausiasSkaiciusSarase([-4, -5, -6, -7, -1, -2, -3, -8]) === -1, 'Didziausia reiksme: -1');
+    console.assert(didziausiasSkaiciusSarase([-2, -3, -4, -5, -6, -7, -8]) === -2, 'Didziausia reiksme: -2');
+    console.assert(didziausiasSkaiciusSarase([-3, -4, -5, -2, -6, -7, -8]) === -2, 'Didziausia reiksme: -2');
+    console.assert(didziausiasSkaiciusSarase([-3.25, -4.25, -5.25, -2, -6.77, -7.77, -8]) === -2, 'Didziausia reiksme: -2');
+    console.assert(didziausiasSkaiciusSarase([-3.25, -4.25, -5.25, -6.77, -7.77, -8]) === -3.25, 'Didziausia reiksme: -3.25');
+}
 
 
 
