465f4051bab9507009db773b4594c4f50eb1f5b3
Rimantas <belauzas@gmail.com>
2025-02-26 08:48:41 +0200

    Up

--- js/extra/mini-max.js
+++ js/extra/mini-max.js
@@ -3,6 +3,8 @@ function didziausiasSkaiciusSarase(list) {
         return 'Pateikta netinkamo tipo reikšmė.';
     }
 
+
+
     return 0;
 }
 
@@ -40,36 +42,88 @@ function didziausiasSkaiciusSarase(list) {
 
 // Masyvas, kuriame nera didziausio skaiciaus
 {
-    console.assert(didziausiasSkaiciusSarase([]) === 'Pateiktas sąrašas negali būti tuščias.', 'Tuscias masyvas');
-    console.assert(didziausiasSkaiciusSarase([true]) === 'Pateiktas sąrašas negali būti tuščias.', 'Tuscias masyvas');
-    console.assert(didziausiasSkaiciusSarase([true, 5]) === 5, 'Didziausia reiksme: 5');
+    // console.assert(didziausiasSkaiciusSarase([]) === 'Pateiktas sąrašas negali būti tuščias.', 'Tuscias masyvas');
+    // console.assert(
+    //     didziausiasSkaiciusSarase([true]) === 'Sąraše nerasta nei viena tinkama reikšmė.',
+    //     'Netinkama reikšmė (boolean)'
+    // );
+    // console.assert(
+    //     didziausiasSkaiciusSarase(['string']) === 'Sąraše nerasta nei viena tinkama reikšmė.',
+    //     'Netinkama reikšmė (string)'
+    // );
+    // console.assert(
+    //     didziausiasSkaiciusSarase([[]]) === 'Sąraše nerasta nei viena tinkama reikšmė.',
+    //     'Netinkama reikšmė (array)'
+    // );
+    // console.assert(
+    //     didziausiasSkaiciusSarase([{}]) === 'Sąraše nerasta nei viena tinkama reikšmė.',
+    //     'Netinkama reikšmė (object)'
+    // );
+    // console.assert(
+    //     didziausiasSkaiciusSarase([Infinity]) === 'Sąraše nerasta nei viena tinkama reikšmė.',
+    //     'Netinkama reikšmė (Infinity)'
+    // );
+    // console.assert(
+    //     didziausiasSkaiciusSarase([-Infinity]) === 'Sąraše nerasta nei viena tinkama reikšmė.',
+    //     'Netinkama reikšmė (-Infinity)'
+    // );
+    // console.assert(
+    //     didziausiasSkaiciusSarase([NaN]) === 'Sąraše nerasta nei viena tinkama reikšmė.',
+    //     'Netinkama reikšmė (NaN)'
+    // );
+    // console.assert(
+    //     didziausiasSkaiciusSarase([undefined]) === 'Sąraše nerasta nei viena tinkama reikšmė.',
+    //     'Netinkama reikšmė (undefined)'
+    // );
+    // console.assert(
+    //     didziausiasSkaiciusSarase([null]) === 'Sąraše nerasta nei viena tinkama reikšmė.',
+    //     'Netinkama reikšmė (null)'
+    // );
+    // console.assert(
+    //     didziausiasSkaiciusSarase([didziausiasSkaiciusSarase]) === 'Sąraše nerasta nei viena tinkama reikšmė.',
+    //     'Netinkama reikšmė (function)'
+    // );
+}
+
+// Masyvas, kuriame apart didziausio skaiciaus yra ir kitu netinkamu reiksmiu
+{
+    // console.assert(didziausiasSkaiciusSarase([true, 5]) === 5, 'Didziausia reiksme: 5 (boolean)');
+    // console.assert(didziausiasSkaiciusSarase(['string', 5]) === 5, 'Didziausia reiksme: 5 (string)');
+    // console.assert(didziausiasSkaiciusSarase([[], 5]) === 5, 'Didziausia reiksme: 5 (array)');
+    // console.assert(didziausiasSkaiciusSarase([{}, 5]) === 5, 'Didziausia reiksme: 5 (object)');
+    // console.assert(didziausiasSkaiciusSarase([Infinity, 5]) === 5, 'Didziausia reiksme: 5 (Infinity)');
+    // console.assert(didziausiasSkaiciusSarase([-Infinity, 5]) === 5, 'Didziausia reiksme: 5 (-Infinity)');
+    // console.assert(didziausiasSkaiciusSarase([NaN, 5]) === 5, 'Didziausia reiksme: 5 (NaN)');
+    // console.assert(didziausiasSkaiciusSarase([undefined, 5]) === 5, 'Didziausia reiksme: 5 (undefined)');
+    // console.assert(didziausiasSkaiciusSarase([null, 5]) === 5, 'Didziausia reiksme: 5 (null)');
+    // console.assert(didziausiasSkaiciusSarase([didziausiasSkaiciusSarase, 5]) === 5, 'Didziausia reiksme: 5 (function)');
 }
 
 // Sarasai su teigiamais (ne neigiami) skaiciais
 {
     console.assert(didziausiasSkaiciusSarase([1]) === 1, 'Grazinamas vienintelis masyve esantis skaicius: 1');
-    console.assert(didziausiasSkaiciusSarase([5]) === 5, 'Grazinamas vienintelis masyve esantis skaicius: 5');
-    console.assert(didziausiasSkaiciusSarase([3, 2, 1]) === 3, 'Masyvo pirmutinis skaicius: 3');
-    console.assert(didziausiasSkaiciusSarase([1, 3, 2]) === 3, 'Masyvo "vidurinis" skaicius: 3');
-    console.assert(didziausiasSkaiciusSarase([1, 2, 3]) === 3, 'Masyvo paskutinis skaicius: 3');
-    console.assert(didziausiasSkaiciusSarase([78, -5, 14, 0, 18]) === 78, 'Masyvo pirmutinis skaicius: 78');
-    console.assert(didziausiasSkaiciusSarase([-5, 78, 14, 0, 18]) === 78, 'Masyvo "vidurinis" skaicius: 78');
-    console.assert(didziausiasSkaiciusSarase([-5, 14, 0, 18, 78]) === 78, 'Masyvo paskutinis skaicius: 78');
-    console.assert(didziausiasSkaiciusSarase([69, 69, 69, 69]) === 69, 'Masyvas tik su vienodomis reiksmemis');
-    console.assert(didziausiasSkaiciusSarase([69, 69, 69, 69, 66]) === 69, 'Pasikartojanti didziausia reiksme');
-    console.assert(didziausiasSkaiciusSarase([69, 69, 69, 69, 66, 66]) === 69, 'Pasikartojanti didziausia reiksme');
-    console.assert(didziausiasSkaiciusSarase([0, 3.14, 2.72, 4]) === 4, 'Didziausia reiksme: 4');
-    console.assert(didziausiasSkaiciusSarase([0, 3.14, 2.72]) === 4, 'Didziausia reiksme: 3.14');
+    // console.assert(didziausiasSkaiciusSarase([5]) === 5, 'Grazinamas vienintelis masyve esantis skaicius: 5');
+    // console.assert(didziausiasSkaiciusSarase([3, 2, 1]) === 3, 'Masyvo pirmutinis skaicius: 3');
+    // console.assert(didziausiasSkaiciusSarase([1, 3, 2]) === 3, 'Masyvo "vidurinis" skaicius: 3');
+    // console.assert(didziausiasSkaiciusSarase([1, 2, 3]) === 3, 'Masyvo paskutinis skaicius: 3');
+    // console.assert(didziausiasSkaiciusSarase([78, -5, 14, 0, 18]) === 78, 'Masyvo pirmutinis skaicius: 78');
+    // console.assert(didziausiasSkaiciusSarase([-5, 78, 14, 0, 18]) === 78, 'Masyvo "vidurinis" skaicius: 78');
+    // console.assert(didziausiasSkaiciusSarase([-5, 14, 0, 18, 78]) === 78, 'Masyvo paskutinis skaicius: 78');
+    // console.assert(didziausiasSkaiciusSarase([69, 69, 69, 69]) === 69, 'Masyvas tik su vienodomis reiksmemis');
+    // console.assert(didziausiasSkaiciusSarase([69, 69, 69, 69, 66]) === 69, 'Pasikartojanti didziausia reiksme');
+    // console.assert(didziausiasSkaiciusSarase([69, 69, 69, 69, 66, 66]) === 69, 'Pasikartojanti didziausia reiksme');
+    // console.assert(didziausiasSkaiciusSarase([0, 3.14, 2.72, 4]) === 4, 'Didziausia reiksme: 4');
+    // console.assert(didziausiasSkaiciusSarase([0, 3.14, 2.72]) === 4, 'Didziausia reiksme: 3.14');
 }
 
 // Sarasai su neigiamais (ne teigiami) skaiciais
 {
-    console.assert(didziausiasSkaiciusSarase([-1, -2, -3, -4, -5, -6, -7, -8]) === -1, 'Didziausia reiksme: -1');
-    console.assert(didziausiasSkaiciusSarase([-4, -5, -6, -7, -1, -2, -3, -8]) === -1, 'Didziausia reiksme: -1');
-    console.assert(didziausiasSkaiciusSarase([-2, -3, -4, -5, -6, -7, -8]) === -2, 'Didziausia reiksme: -2');
-    console.assert(didziausiasSkaiciusSarase([-3, -4, -5, -2, -6, -7, -8]) === -2, 'Didziausia reiksme: -2');
-    console.assert(didziausiasSkaiciusSarase([-3.25, -4.25, -5.25, -2, -6.77, -7.77, -8]) === -2, 'Didziausia reiksme: -2');
-    console.assert(didziausiasSkaiciusSarase([-3.25, -4.25, -5.25, -6.77, -7.77, -8]) === -3.25, 'Didziausia reiksme: -3.25');
+    // console.assert(didziausiasSkaiciusSarase([-1, -2, -3, -4, -5, -6, -7, -8]) === -1, 'Didziausia reiksme: -1');
+    // console.assert(didziausiasSkaiciusSarase([-4, -5, -6, -7, -1, -2, -3, -8]) === -1, 'Didziausia reiksme: -1');
+    // console.assert(didziausiasSkaiciusSarase([-2, -3, -4, -5, -6, -7, -8]) === -2, 'Didziausia reiksme: -2');
+    // console.assert(didziausiasSkaiciusSarase([-3, -4, -5, -2, -6, -7, -8]) === -2, 'Didziausia reiksme: -2');
+    // console.assert(didziausiasSkaiciusSarase([-3.25, -4.25, -5.25, -2, -6.77, -7.77, -8]) === -2, 'Didziausia reiksme: -2');
+    // console.assert(didziausiasSkaiciusSarase([-3.25, -4.25, -5.25, -6.77, -7.77, -8]) === -3.25, 'Didziausia reiksme: -3.25');
 }
 
 
